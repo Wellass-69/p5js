dc03a4fb2e10b28115762a6d41bf7b2ee54fc9dd
Wellass-69 <lunamorenobreno@gmail.com>
2024-09-09 07:42:55 -0300

    Create skatch.js

---
+++ skatch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500,  500);
+   background("turquoise");
+}
+
+function draw() {
+  stroke("black") //borda;
+ fill("red");
+ //console.log(mouseIsPressed);
+ 
+  if(mouseIsPressed);
+   square(mouseX,mouseY,90,90)
+}
